48a83df513fc6b0ef8912adfcf67e535d52828c6
tomarcodinglife <tomarcodinglife@gmail.com>
2024-11-28 08:30:50 +0530

    updated

---
+++ 09_oop/02_class.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <list>
+#include <vector>
+
+using namespace std;
+
+
+class tea {
+    // data members
+    string teaName; // name of the tea
+    int servings; // Number of servings
+    vector<string> ingredients; //list of ingredients for the tea
+};
+
+
+int main() {
+
+    return 0;
+}
