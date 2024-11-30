7c21950f2c046d24b633823094dd9dfcadc1deb9
tomarcodinglife <tomarcodinglife@gmail.com>
2024-11-30 08:38:03 +0530

    updated

--- 09_oop/02_class.cpp
+++ 09_oop/02_class.cpp
@@ -6,15 +6,34 @@
 using namespace std;
 
 
-class tea {
-    // data members
-    string teaName; // name of the tea
-    int servings; // Number of servings
-    vector<string> ingredients; //list of ingredients for the tea
+class Tea {
+    public:
+        // data members
+        string teaName; // name of the tea
+        int servings; // Number of servings
+        vector<string> ingredients; //list of ingredients for the tea
+
+        void displayTeaDetails() {
+            cout << "Tea Name : " << teaName << endl;
+            cout << "Servings : " << servings << endl;
+            cout << "ingredient : ";
+            for (string ingredient : ingredients){
+                cout << ingredient << " ";
+            }
+            cout<<endl;
+        }
 };
 
 
 int main() {
+    /*Tea tea you can write any name of object but many times you can see same name but remember start from capital letter is class.*/
+    Tea teaObject; 
+
+    teaObject.teaName = "Lemon Tea";
+    teaObject.servings = 5;
+    teaObject.ingredients = {"Water", "Lomon", "Honey Sugar", "Tea Leaves" };
+
+    teaObject.displayTeaDetails();
 
     return 0;
 }
