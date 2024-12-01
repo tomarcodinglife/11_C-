5eaa5d2fe70dc3d0111d4a993fe775601b42956b
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-01 13:31:03 +0530

    updated

---
+++ test/02_test.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+
+using namespace std; 
+
+   int myfun(int anyArray[], int size){
+        total = 0;
+        
+        for (int i=0 ; i<size ; i++){
+            total += anyArray[0];
+
+            anyArray.
+
+            return total;
+
+        }
+    
+
+   }
+
+int main() {
+
+    int array[10] = {10, 20, 30, 40, 50, 60};
+
+ 
+        
+
+    return 0;
+}
