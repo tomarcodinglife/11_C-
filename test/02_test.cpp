85dd613c5c850593fe9f5d5a8404646003752078
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-01 14:24:05 +0530

    updated

--- test/02_test.cpp
+++ test/02_test.cpp
@@ -3,27 +3,37 @@
 
 using namespace std; 
 
-   int myfun(int anyArray[], int size){
-        total = 0;
-        
+   int myArrayfun(int anyArray[], int size){
+        int total = 0;
         for (int i=0 ; i<size ; i++){
-            total += anyArray[0];
+            total += anyArray[i];
 
-            anyArray.
-
-            return total;
-
+            cout<< " current item \n"<< anyArray[i];
+            // return total;
         }
-    
+        cout<<"the loop end"<<endl;
+        return total;
+   }
 
+
+   int teaSell(int arrayDay[], int length){
+        int totalSell = 0;
+
+        for(int i=0; i<length ; i++){
+            totalSell += arrayDay[i];
+            cout << " tea sell "<<arrayDay[i]<<" at current Day \n";
+        }
+        cout << " total tea sell " << totalSell << " Thank You"<<endl;
+        return totalSell;
    }
 
 int main() {
 
-    int array[10] = {10, 20, 30, 40, 50, 60};
+    // int array[10] = {10, 20, 30, 40, 50, 60};
+    // myArrayfun(array, 6);
 
- 
-        
+    int sell[5] = {18, 15, 31, 60, 45};
+    teaSell(sell, 5);
 
     return 0;
 }
