4c60ed58b35de0013b322e3d77b768b4e1c451bd
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-01 18:09:07 +0530

    updated

--- test/03_test.cpp
+++ test/03_test.cpp
@@ -3,10 +3,19 @@
 
 using namespace std;
 
+int* myMethod(int cups){
+    int* sell = new int[cups];
 
-
+    for (int i=0; i<cups ;i++){
+        sell += i + 1;
+        cout<<" today sell is "<< sell[i] << endl;
+    }
+    return sell;
+}   
 
 int main() {
+
+    int array[] = ;
 
     return 0;
 }
