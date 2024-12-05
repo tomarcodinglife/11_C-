2a02a4dcd1ab7f289582f77d058b379b30946951
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-05 11:24:52 +0530

    updated

--- test/06_test.cpp
+++ test/06_test.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-class studentDetails{
+class StudentDetails{
     // data member or attributes
     public:
             string firstName;
@@ -13,32 +13,34 @@ class studentDetails{
             int rollNumber;
             bool isPass;
             float marksPercent;
-            vector <int> marksinSubjets;
+            vector<int> marksinSubjets;
 
             //Member Function
+            
             void displayStudentDetails(){
                 cout << " ------------------- Display Student Details Below ----------------------"<<endl;
-                cout << " Student Name is "<< firstName << lastName <<endl;
-                cout << firstName << " " <<lastName <<" Roll Number is  " << rollNumber <<endl;
-                cout << firstName << " " <<lastName <<" Pass status is  " << isPass<< " with "<< marksPercent<< " % " <<endl;
+                cout << " Student Name is "<< firstName << " " << lastName <<endl;
+                cout << firstName << " " << lastName <<" Roll Number is  " << rollNumber <<endl;
+                // cout << firstName<<lastName <<" Pass status is  " << "with"<< marksPercent<< " % " <<endl;
+                cout<< firstName << " " << lastName << " is Pass status is " << (isPass? "Passed":"Failed")<< " with " << marksPercent << " % "<<endl;
                 cout << " Marks subject wise is ";
-                for (int marks : marksinSubjets){
-                    cout<< marks <<" ";
-                    for (int i=0 ; i < sizeof(marksinSubjets); i++){
-                        cout<< "------------- Display Student Marks-------------"<<endl;
-                        cout << "Marks in Subject is " << marks<< endl;
-                        cout<< "------------- Display Student Marks End-------------"<<endl;
-                    }
-                } 
+                    for (int marks : marksinSubjets){
+                        cout<< marks <<" ";
+                    } 
                 cout<< endl;
-                cout << " ------------------------- End Student Details --------------------------"<<endl;
+                cout << "------------------------- End Student Details --------------------------" << endl;
+
             }
         };
 
 int main(){
-
-
-
-
+    StudentDetails studentDetails;
+    studentDetails.firstName = " Sujit ";
+    studentDetails.lastName = " Tomar ";
+    studentDetails.rollNumber = 189;
+    studentDetails.isPass = true;
+    studentDetails.marksPercent = 89.88;
+    studentDetails.marksinSubjets = {69, 55, 89, 40, 60, 88};
+    studentDetails.displayStudentDetails();
     return 0;
 }
