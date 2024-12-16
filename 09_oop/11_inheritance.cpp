cb89fa8aca45d2653e7791a7dfc6e308bc020c39
tomarcodinglife <tomarcodinglife@gmail.com>
2024-12-16 19:18:58 +0530

    updated

--- 09_oop/11_inheritance.cpp
+++ 09_oop/11_inheritance.cpp
@@ -33,27 +33,59 @@ class Tea {
 
 class GreenTea : public Tea {
     public :
+        // virtual method 
         GreenTea(int serv) : Tea("Green Tea", serv){
             cout << "Green Tea Constructor Called"<<endl;
         }
+        // tea class's brew method overide
         void brew() const override{
             cout << "Brewing" << teaName << "by steeping tea leaves" <<endl;
         }
 
-        //
+        //Virtual Destructor Method
         ~GreenTea(){
-            cout << "Green tea constructor called "<< endl;
+            cout << "Green tea Destructor called "<< endl;
         }
 };
 
 class MasalaTea : public Tea {
     public :
+        //Constructor Method
+        MasalaTea(int serv) : Tea("Masala Tea", serv){
+            cout << "Masala Tea Constructor Called"<<endl;
+        }
 
-
+        //tea class's brew method overide
+        void brew() const override final { // when you write method with final it means you can't overide this method.
+            cout << "Brewing" << teaName << "by steeping tea leaves, masala with milk"<<endl;
+        }
+        //
+        ~MasalaTea(){
+            cout << "Masala Tea Destructor Called for Clear Memory"<<endl;
+        }
 };
 
-int main(){
+/*
+class spicyMasalaTea : public MasalaTea {
+    public :
+        void brew() const override { // here you are wrong because you override method who create with final keyword in  masala tea
 
+        }
+} ;
+*/
+
+int main(){
+    Tea* tea1 = new GreenTea(2);
+    Tea* tea2 = new MasalaTea(5);
+
+    tea1 -> brew();
+    tea1 -> serve();
+
+    tea2 -> brew();
+    tea2 -> serve();
+
+    delete tea1;
+    delete tea2;
 
     return 0;
 }
